8a93b6573ad3ba45a3730fb0c666c8da7a757fdb
Jorge del Rio <jorge.delrio@kueski.com>
2017-05-24 14:09:09 -0500

    Add new filters to inclusion/exclusion of files

--- lib/rubypack/rubypack_file.rb
+++ lib/rubypack/rubypack_file.rb
@@ -32,13 +32,25 @@ module Rubypack
     end
 
     def include(*args)
+      args.flatten!
       @rules = [] unless defined?(@rules)
       args.each { |entry| @rules << { action: :include, filter: entry } }
     end
 
     def exclude(*args)
+      args.flatten!
       @rules = [] unless defined?(@rules)
       args.each { |entry| @rules << { action: :exclude, filter: entry } }
+    end
+
+    def all
+      '**/**'
+    end
+
+    def git
+      Dir.chdir(@path) do
+        `git ls-files -z`.split("\x0")
+      end
     end
 
     def format_rule(rule, files)
@@ -50,44 +62,44 @@ module Rubypack
     def list_files
       Dir.chdir(@path) do
 
-      # If not rules were defined, include all by default
-      unless defined?(@rules)
-        files = Dir['**/**']        
-        format_rule({ action: :include, filter: '**/**' }, files)
-        return ffiles
-      end
-
-      # Determinate if the package must start with all files or with none
-      action = @rules.first[:action]
-      if action == :include
-        files = []
-      elsif action == :exclude
-        files = Dir['**/**']
-        format_rule({ action: :include, filter: '**/**' }, files)
-      else
-        fail("Action not implemented: #{action}")
-      end
-
-      # Include/exclude based on the rules
-      @rules.each do |rule|
-        if rule[:action] == :include
-          filtered_files = Dir[rule[:filter]]
-          files |= filtered_files
-          format_rule(rule, filtered_files)
-        elsif rule[:action] == :exclude
-          filtered_files = Dir[rule[:filter]]
-          files -= filtered_files
-          format_rule(rule, filtered_files)
-        else
-          fail("Action not implemented: #{rule[:action]}")
+        # If not rules were defined, include all by default
+        unless defined?(@rules)
+          files = Dir['**/**']        
+          format_rule({ action: :include, filter: '**/**' }, files)
+          return ffiles
         end
-      end
 
-      # Remove directories
-      files.reject! { |file| File.directory?(file) }
+        # Determinate if the package must start with all files or with none
+        action = @rules.first[:action]
+        if action == :include
+          files = []
+        elsif action == :exclude
+          files = Dir['**/**']
+          format_rule({ action: :include, filter: '**/**' }, files)
+        else
+          fail("Action not implemented: #{action}")
+        end
 
-      # Sort the file names
-      files.sort
+        # Include/exclude based on the rules
+        @rules.each do |rule|
+          if rule[:action] == :include
+            filtered_files = Dir[rule[:filter]]
+            files |= filtered_files
+            format_rule(rule, filtered_files)
+          elsif rule[:action] == :exclude
+            filtered_files = Dir[rule[:filter]]
+            files -= filtered_files
+            format_rule(rule, filtered_files)
+          else
+            fail("Action not implemented: #{rule[:action]}")
+          end
+        end
+
+        # Remove directories
+        files.reject! { |file| File.directory?(file) }
+
+        # Sort the file names
+        files.sort
       end
     end
 
